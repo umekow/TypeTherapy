6f9e2733aab3e28f11d91536b1bb5a5fb7cb2cfc
umekow <umekolashun@gmail.com>
2019-12-27 23:27:34 -0600

    skeleton for User component

--- frontend/src/components/journal/User.js
+++ frontend/src/components/journal/User.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 //component that contains summary for user profile such as username, link to account settings, and icon
-const User = () => {
-
+const User = props => {
+    return(
+        <div className="user">
+            <p className="username"> Welcome, {props.username} </p> 
+            <Link to="/account">Your Account</Link>
+        </div>
+    )
 }
 
 export default User
