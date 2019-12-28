656db786e9885187b154df14c7e85fa5dd1133e7
umekow <umekolashun@gmail.com>
2019-12-27 23:10:42 -0600

    skeleton for month component

--- frontend/src/components/journal/Month.js
+++ frontend/src/components/journal/Month.js
@@ -1,9 +1,24 @@
 import React from "react"; 
+import DayEntry from './DayEntry'; 
 
 const Month = props => {
     return(
-        <div className="month">
-            {props.month}
+        <div className="month-container">
+            
+            <div className="month">
+                {props.month}
+            </div>
+
+            {
+                props.entries.map(entry => <DayEntry 
+                                                date={entry.date} 
+                                                day={entry.day} 
+                                                title={entry.title} 
+                                                summary={entry.summary}
+                                            />
+                )
+            }
+        
         </div>
     )
 }
