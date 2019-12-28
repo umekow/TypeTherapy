17f499ac2a9282eaed51d2c7b7876803df67939c
umekow <umekolashun@gmail.com>
2019-12-27 23:04:34 -0600

    created new component called month

---
+++ frontend/src/components/journal/Month.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+const Month = props => {
+    return(
+        <div className="month">
+            {props.month}
+        </div>
+    )
+}
+
+
+export default Month; 
