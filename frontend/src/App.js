91bf7a9daa3437b1b44047f9dc54b9d59339990b
umekow <umekolashun@gmail.com>
2020-01-07 17:35:12 -0600

    worked on sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,50 +3,9 @@ import './App.css';
 
 //components
 import Editor from './components/journal/editor/Editor.js'; 
-import Month from './components/journal/sidebar/months/Month'
 
-const months = [
-  { 
-    "month": 'January', 
-    "entries":
-      [
-        {
-          weekday : 'WED', 
-          day : 1, 
-          year : 2020, 
-          title: 'a new start', 
-          summary : "I went to the gym and the trainer reassured me that it was a judgement free zone", 
 
-      }, 
-      {
-        weekday : 'THU', 
-        day : 2, 
-        year : 2020, 
-        title: 'keep going', 
-        summary : "I was so afraid that people would look at me. Luckily my sister went with me...", 
 
-      }, 
-      {
-
-        weekday : 'FRI', 
-        day : 3, 
-        year : 2020, 
-        title: 'I can not run on the treadmill', 
-        summary : "What was the trainer thinking? Did he really think I could run at 5 speed??", 
-
-      }, 
-      {
-        weekday : 'SAT', 
-        day : 4, 
-        year : 2020, 
-        title: 'Glutes!!', 
-        summary : "My brother went to the gym with me that made me comfortable to try weight training. I love working on my glutes!!", 
-
-      } 
-    ]
-}
-
-]
 function App() {
   return (
     <div className="App">
--- frontend/src/components/journal/sidebar/SideBar.js
+++ frontend/src/components/journal/sidebar/SideBar.js
@@ -1,13 +1,58 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 //sub-components
 import User from './User'; 
 import Month from './months/Month'
 //container for every component that will be display on the side such as a list of all entries and user summary
 const SideBar = props => {
+    const [username, setUsername] = useState('user'); 
+    const [entries, setEntries] = useState( [
+        { 
+          "month": 'January', 
+          "entries":
+            [
+              {
+                weekday : 'WED', 
+                day : 1, 
+                year : 2020, 
+                title: 'a new start', 
+                summary : "I went to the gym and the trainer reassured me that it was a judgement free zone", 
+      
+            }, 
+            {
+              weekday : 'THU', 
+              day : 2, 
+              year : 2020, 
+              title: 'keep going', 
+              summary : "I was so afraid that people would look at me. Luckily my sister went with me...", 
+      
+            }, 
+            {
+      
+              weekday : 'FRI', 
+              day : 3, 
+              year : 2020, 
+              title: 'I can not run on the treadmill', 
+              summary : "What was the trainer thinking? Did he really think I could run at 5 speed??", 
+      
+            }, 
+            {
+              weekday : 'SAT', 
+              day : 4, 
+              year : 2020, 
+              title: 'Glutes!!', 
+              summary : "My brother went to the gym with me that made me comfortable to try weight training. I love working on my glutes!!", 
+      
+            } 
+          ]
+      }
+      
+      ]); 
+
+
     return (
         <div className="sideBar">
-            <User/>
-           {props.months.map(month => <Month month={month.month} entries={month.entries}/>)}
+            <User username={username}/>
+          
         </div>
     )
 }
